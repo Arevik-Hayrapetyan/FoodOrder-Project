403b6cc8ef9a67740c43f08942554499a51622b9
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-01-15 00:55:46 +0400

    feat: Added menu-items html and main.js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,8 @@
-// // const btnMenu = document.querySelector(".menu-icon");
-// // btnMenu.addEventListener("click", function () {
-// //  console.log("hi")
-// // });
-// const menu = document.querySelector(".menu");
-// const menuItems = document.querySelectorAll(".menu-item");
-// const menuIcons = document.querySelector(".menu-icons");
-// const closeIcon = document.querySelector(".close-con");
-// const menuIcon = document.querySelector(".menu-icon");
-
-// function toggleMenu() {
-//   if (menu.classList.contains("show-menu")) {
-//     menu.classList.remove("show-menu");
-//     closeIcon.style.display = "none";
-//     menuIcon.style.display = "block";
-//   } else {
-//     menu.classList.add("show-menu");
-//     closeIcon.style.display = "block";
-//     menuIcon.style.display = "none";
-//   }
-// }
-
-// menuIcon.addEventListener("click", toggleMenu);
-// // menuItems.forEach(function (menuItem) {
-// //   menuItem.addEventListener("click", toggleMenu);
-// // });
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menu-item");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".close-icon");
-const menuIcon = document.querySelector(".menu-icon");
-
-function toggleMenu() {
-  if (menu.classList.contains("show-menu")) {
-    menu.classList.remove("show-menu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("show-menu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener("click", toggleMenu);
-});
+// import { setItems } from "../helpers/localStorage";
+// const select = document.querySelector(".table-order");
+// const optionValue = select.options[select.selectedIndex].text;
+// console.log(optionValue);
+// setItems("user", 2);
+// function getTableNum() {}
+// const orderBtn = document.querySelector(".order-btn");
+// orderBtn.addEventListener("click", getTableNum);
