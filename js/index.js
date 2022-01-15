a5874863f6056b7a67e338a5e3b89b9af37c04ee
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-01-15 13:32:36 +0400

    feast: disable button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-const foodTitle = document.getElementById("content").textContent;
+const orderList = document.querySelector(".order-list");
+const addButton = document.querySelector(".add-btn");
+const input = document.querySelector(".counts");
+const foodName = document.querySelector(".food-name").textContent;
+const addCountBtn = document.getElementById("addCountBtn");
+const subCountBtn = document.getElementById("subCountBtn");
+
+addButton.addEventListener("click", addOrder);
+addCountBtn.addEventListener("click", addCount);
+subCountBtn.addEventListener("click", subCount);
+
+function getFoodName() {
+  return foodName;
+}
+
+function addOrder(event) {
+  event.preventDefault();
+  const orderDiv = document.createElement("div");
+  orderDiv.classList("order");
+  const newOrder = document.createElement("li");
+  newOrder.innerText = foodName;
+  newOrder.classList.add("order-item");
+  orderDiv.appendChild(newOrder);
+}
+function addCount() {
+  input.value++;
+  subCountBtn.disabled = false;
+}
+function subCount() {
+  if (input.value) {
+    subCountBtn.disabled = true;
+  }
+  subCountBtn = false;
+  input.value--;
+}
